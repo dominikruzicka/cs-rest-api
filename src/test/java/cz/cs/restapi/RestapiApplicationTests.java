500c44e069ee4ce14fb61159ce56abe25b72aced
Dominik Ruzicka <dominikruzicka323@gmail.com>
2021-11-11 15:10:10 +0100

    generate App, ready to start development

---
+++ src/test/java/cz/cs/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cs.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
