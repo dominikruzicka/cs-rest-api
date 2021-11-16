92ba485fe685386ad0a459e81503dd80260eff45
Dominik Ruzicka <dominikruzicka323@gmail.com>
2021-11-17 00:12:23 +0100

    create code to get Set of Senders account from Transaction, fixed AccountsResource class to iterate through returned pages

---
+++ src/main/java/cz/cs/restapi/call/model/Sender.java
@@ -0,0 +1,85 @@
+package cz.cs.restapi.call.model;
+
+public class Sender {
+    private String accountNumber;
+    private String bankCode;
+    private String iban;
+    private String specificSymbol;
+    private String specificSymbolParty;
+    private String variableSymbol;
+    private String constantSymbol;
+    private String name;
+    private String description;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getBankCode() {
+        return bankCode;
+    }
+
+    public void setBankCode(String bankCode) {
+        this.bankCode = bankCode;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getSpecificSymbol() {
+        return specificSymbol;
+    }
+
+    public void setSpecificSymbol(String specificSymbol) {
+        this.specificSymbol = specificSymbol;
+    }
+
+    public String getSpecificSymbolParty() {
+        return specificSymbolParty;
+    }
+
+    public void setSpecificSymbolParty(String specificSymbolParty) {
+        this.specificSymbolParty = specificSymbolParty;
+    }
+
+    public String getVariableSymbol() {
+        return variableSymbol;
+    }
+
+    public void setVariableSymbol(String variableSymbol) {
+        this.variableSymbol = variableSymbol;
+    }
+
+    public String getConstantSymbol() {
+        return constantSymbol;
+    }
+
+    public void setConstantSymbol(String constantSymbol) {
+        this.constantSymbol = constantSymbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
